445de046f4abd628073ce26e8da516e73f4908d5
Usarneme <jamestcoop@gmail.com>
2021-10-07 11:00:54 -0700

    adds functionality to create powerpoint slide with images, title, and text and save (via interop).

--- HelloWpf/Greetings.xaml.cs
+++ HelloWpf/Greetings.xaml.cs
@@ -2,26 +2,27 @@
 using PexelsDotNetSDK.Models;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using Application = Microsoft.Office.Interop.PowerPoint.Application;
 
 namespace HelloWpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    /// pexels
-    /// 563492ad6f917000010000019a625093f14641ecb876ff0b65cc8627
     public partial class MainWindow : Window
     {
         private int NUMBER_OF_IMAGES = 8;
+        public List<string> imageIds = new List<string>();
+        public Dictionary<string, string> imageUrlById = new Dictionary<string, string>();
 
         public MainWindow()
         {
@@ -35,7 +36,6 @@ namespace HelloWpf
             {
                 foreach (Run r in p.Inlines)
                 {
-                    //Debug.WriteLine("r.text {0}. r.fontweight {1}", r.Text, r.FontWeight);
                     if (r.FontWeight == FontWeights.Bold)
                     {
                         boldWords.Add(r.Text);
@@ -52,39 +52,29 @@ namespace HelloWpf
             PexelsClient _pex = new PexelsClient("563492ad6f917000010000019a625093f14641ecb876ff0b65cc8627");
             string boldWords = StringOfBoldWordsFromRichTextBox(TextText);
             string query = TitleText.Text + " " + boldWords;
-            Debug.WriteLine("Button clicked...getting results of query: {0}", query);
             PhotoPage result = await _pex.SearchPhotosAsync(query, null, "medium", null, null, 1, NUMBER_OF_IMAGES);
-            //Debug.WriteLine("Got result from pexels: {0} - length: {1}", result, result.photos.Count);
-            StackPanel sp;
             Border border;
 
             if (result.photos.Count == 0)
             {
                 ErrorHolder.Text = "No photos found!";
+                Slide_Button.IsEnabled = false;
             }
             else
             {
                 int index = 0;
                 foreach (Photo p in result.photos)
                 {
+                    string name = "id_" + p.id;
+                    imageUrlById.Add(name, p.source.medium);
                     int row = (int)Math.Floor((double)index / 4);
                     int col = index % 4;
-                    //Debug.WriteLine("ROW {0}, COL {1}, image: {2}", row, col, p.url);
-                    //sp = new StackPanel();
-                    //sp.SetValue(Grid.RowProperty, row);
-                    //sp.SetValue(Grid.ColumnProperty, col);
-                    //sp.SetValue(NameProperty, "id_" + p.id);
-                    //sp.AddHandler(MouseUpEvent, new MouseButtonEventHandler(Handle_Click));
 
                     border = new Border();
                     border.SetValue(Grid.RowProperty, row);
                     border.SetValue(Grid.ColumnProperty, col);
-                    border.SetValue(NameProperty, "id_" + p.id);
+                    border.SetValue(NameProperty, name);
                     border.AddHandler(MouseUpEvent, new MouseButtonEventHandler(Handle_Click));
-
-                    Thickness t = new Thickness(5);
-                    border.SetValue(BorderThicknessProperty, t);
-                    border.SetValue(BorderBrushProperty, Brushes.Red);
 
                     Image _pic = new Image();
                     BitmapImage _myBm = new BitmapImage();
@@ -93,31 +83,62 @@ namespace HelloWpf
                     _myBm.DecodePixelWidth = 200;
                     _myBm.EndInit();
                     _pic.Source = _myBm;
-                    //sp.Children.Add(_pic);
-                    //MyGrid.Children.Add(sp);
+
                     border.Child = _pic;
                     MyGrid.Children.Add(border);
                     index++;
                 }
+                Slide_Button.IsEnabled = true;
             }
         }
 
-        public void Handle_Click(object sender, RoutedEventArgs e)
+        private void Handle_Click(object sender, RoutedEventArgs e)
         {
             Border that = (Border)sender;
-            Debug.WriteLine("Image clicked on: {0} {1}", that.Name, that.Style);
-            Thickness t = new Thickness(5);
-            that.SetValue(BorderThicknessProperty, t);
-            that.SetValue(BorderBrushProperty, Brushes.Green);
-            //Type myType = sender.GetType();
-            //Debug.WriteLine("Got type of {0}", myType);
-            //IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
-            //foreach (PropertyInfo p in props)
-            //{
-            //    object propValue = p.GetValue(sender, null);
-            //    Debug.WriteLine("Got keys {0} and values {1}", p, propValue);
-            //}
-            // Got keys System.String Name and values id_4672553
+            if (that.GetValue(BorderBrushProperty) == null)
+            {
+                Thickness t = new Thickness(5);
+                that.SetValue(BorderThicknessProperty, t);
+                that.SetValue(BorderBrushProperty, Brushes.Green);
+                imageIds.Add(that.Name);
+            }
+            else
+            {
+                Thickness t = new Thickness(0);
+                that.SetValue(BorderThicknessProperty, t);
+                that.SetValue(BorderBrushProperty, null);
+                imageIds.Remove(that.Name);
+            }
+        }
+
+        private void Create_Slide(object sender, RoutedEventArgs e)
+        {
+            Application pptApp = new Application();
+            var pptPresentation = pptApp.Presentations.Add(MsoTriState.msoTrue);
+            var customLayout = pptPresentation.SlideMaster.CustomLayouts[PpSlideLayout.ppLayoutPictureWithCaption];
+            Slides slides;
+            _Slide slide;
+            PowerPoint.TextRange objText;
+
+            slides = pptPresentation.Slides;
+            slide = pptPresentation.Slides.AddSlide(1, customLayout);
+
+            objText = slide.Shapes[1].TextFrame.TextRange;
+            objText.Text = TitleText.Text;
+            objText.Font.Size = 32;
+
+            objText = slide.Shapes[2].TextFrame.TextRange;
+            System.Windows.Documents.TextRange txt = new System.Windows.Documents.TextRange(TextText.Document.ContentStart, TextText.Document.ContentEnd);
+            objText.Text = txt.Text;
+
+            foreach (string xamlName in imageIds)
+            {
+                string url = imageUrlById[xamlName];
+                PowerPoint.Shape shape = slide.Shapes[2];
+                slide.Shapes.AddPicture(url, MsoTriState.msoFalse, MsoTriState.msoTrue, shape.Left, shape.Top, shape.Width, shape.Height);
+            }
+
+            pptPresentation.SaveAs(Name, PpSaveAsFileType.ppSaveAsDefault, MsoTriState.msoTrue);
         }
     }
 }
